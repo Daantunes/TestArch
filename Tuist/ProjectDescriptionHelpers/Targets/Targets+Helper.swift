1bef9eaf7f6564744743a1f51f60f8313a60c08d
nunocaseiro <n.caseiro@kigroup.de>
2023-09-05 15:58:45 +0100

    chore: remove strict

--- Tuist/ProjectDescriptionHelpers/Targets/Targets+Helper.swift
+++ Tuist/ProjectDescriptionHelpers/Targets/Targets+Helper.swift
@@ -19,7 +19,7 @@ public extension Target {
             .post(
                 script: """
                     if which swiftlint >/dev/null; then
-                        swiftlint --strict
+                        swiftlint
                     else
                         echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
                     fi
